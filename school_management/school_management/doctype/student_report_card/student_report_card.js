0f05e2854539578a015f8dd9614130ff9842da37
subinpsn <you@example.com>
2023-09-25 09:55:30 +0530

    inital commit

---
+++ school_management/school_management/doctype/student_report_card/student_report_card.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, subinpsn and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Student Report Card", {
+// 	refresh(frm) {
+
+// 	},
+// });
